09e7e32f4146996c58c5e56afe53580457a04cc9
calvincetom <calvincetom@outlook.com>
2022-10-23 16:31:59 +0300

    add api

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 
 """
 Contains the class TestConsoleDocs
 """
@@ -15,14 +15,14 @@ class TestConsoleDocs(unittest.TestCase):
     def test_pep8_conformance_console(self):
         """Test that console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['console.py'])
+        result = pep8s.check_files(['/console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_pep8_conformance_test_console(self):
         """Test that tests/test_console.py conforms to PEP8."""
         pep8s = pep8.StyleGuide(quiet=True)
-        result = pep8s.check_files(['tests/test_console.py'])
+        result = pep8s.check_files(['/tests/test_console.py'])
         self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
@@ -39,7 +39,3 @@ class TestConsoleDocs(unittest.TestCase):
                          "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
-
-if __name__ == '__main__':
-    #main test_module
-    unittest.main()
